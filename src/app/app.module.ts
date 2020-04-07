2efbf60c2d2c0766476a0cecf1ed7aac5e33130d
Iñaki Agudo <agudovitoria@gmail.com>
2020-04-07 22:20:28 +0200

    First commit Sidenav and Main grid working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MaterialModule } from './common/material.module';
+import { MainGridComponent } from './components/main-grid/main-grid.component';
+import { MainToolbarComponent } from './components/main-toolbar/main-toolbar.component';
+import { MainSidebarComponent } from './components/main-sidebar/main-sidebar.component';
+import { ContentGridComponent } from './components/content-grid/content-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainGridComponent,
+    MainToolbarComponent,
+    MainSidebarComponent,
+    ContentGridComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ]
+  , bootstrap: [AppComponent]
 })
 export class AppModule { }
